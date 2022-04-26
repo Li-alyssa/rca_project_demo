0cec4ed899a7b430e69a45b9d4d353e70b2def49
li-alyssa <791927942@qq.com>
2022-04-26 13:29:58 +0800

    更新第三个页面

--- src/views/ThirdView.vue
+++ src/views/ThirdView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="third">
-    <h1>这里是third界面</h1>
+  <div class="screen-body">
+    <section class="screen-DataPage">
+      <div id="top-title">▎ 节点关系图例</div>
+      <DataPage />
+    </section>
   </div>
 </template>
+<script>
+import DataPage from "@/components/thirdpage/DataPage.vue";
+export default {
+  name: "AboutView",
+  components: {
+    DataPage,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.screen-body {
+  height: 100%;
+  width: 100%;
+  // background-color: red;
+  .screen-DataPage {
+    height: 95%;
+    width: 80%;
+  }
+}
+#top-title {
+  margin-bottom: 0.5%;
+  // background-color: green;
+  color: aliceblue;
+}
+</style>
