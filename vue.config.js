c961e74765fabe1c857edec3ba64a5edd978bc94
litianyu <9961190+brunda@user.noreply.gitee.com>
2022-04-28 11:46:10 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: "/rca_project_demo/"
+  publicPath: "./rca_project_demo/"
 })
